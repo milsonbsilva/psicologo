cecbe2030792de55e52a10af4041f6a38898ed45
anr00 <161641917+anr00@users.noreply.github.com>
2025-03-04 22:14:56 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Configuration;
+
+string conexao_string = ConfigurationManager.AppSettings.Get("ConnectionString");
+
+Gerenciador_Banco_Dados gbd= new();
+Input input = new();
+gbd.NovaTabela(conexao_string);
+input.Menu();
