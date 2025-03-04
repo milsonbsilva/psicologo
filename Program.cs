27a4642c4d5cf24dec6ff4bf1a2e9a49d8777548
anr00 <161641917+anr00@users.noreply.github.com>
2025-03-04 22:17:15 +0000

    Delete Program.cs

--- Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Configuration;
-
-string conexao_string = ConfigurationManager.AppSettings.Get("ConnectionString");
-
-Gerenciador_Banco_Dados gbd= new();
-Input input = new();
-gbd.NovaTabela(conexao_string);
-input.Menu();
